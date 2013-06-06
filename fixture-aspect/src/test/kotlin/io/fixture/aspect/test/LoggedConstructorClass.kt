252828ac397cb0aa0c246f12c788d1a6a51e3808
Martin Lau <m@hum.ph>
2013-06-07 07:17:40 +1000

    Reformatting

--- fixture-aspect/src/test/kotlin/io/fixture/aspect/test/LoggedConstructorClass.kt
+++ fixture-aspect/src/test/kotlin/io/fixture/aspect/test/LoggedConstructorClass.kt
@@ -3,7 +3,10 @@ package io.fixture.aspect.test
 import ch.qos.logback.classic.Logger
 import org.slf4j.LoggerFactory
 
-class LoggedConstructorClass(val message: String, val `object`: Any) {
+class LoggedConstructorClass(
+        val message: String,
+        val `object`: Any
+) {
 
     class object {
         val logger: Logger = LoggerFactory.getLogger(javaClass<LoggedConstructorClass>()) as Logger
