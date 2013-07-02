f76287f9249cd3dba9fed9c898be84b65af227d5
Martin Lau <m@hum.ph>
2013-07-02 21:07:27 +1000

    Added base configuration for user administration

--- fixture-web/src/main/kotlin/io/fixture/web/controller/AdministrationController.kt
+++ fixture-web/src/main/kotlin/io/fixture/web/controller/AdministrationController.kt
@@ -4,9 +4,13 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 
 [Controller]
+[RequestMapping(array("/administration"))]
 public class AdministrationController {
 
-    [RequestMapping(array("/administration"))]
+    [RequestMapping]
     fun index() = ".administration.index"
+
+    [RequestMapping(array("/users"))]
+    fun users() = ".administration.users"
 
 }
